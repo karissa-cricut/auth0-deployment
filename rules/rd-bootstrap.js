a47e97760c597f810e1fac43859bea6edbbfe970
Rudy Dahbura <rdahbura@gmail.com>
2019-08-15 17:02:30 -0700

    Better formatting.

--- rules/rd-bootstrap.js
+++ rules/rd-bootstrap.js
@@ -1,41 +1,34 @@
 function boostrap(user, context, callback) {
   const ManagementClient = require('auth0@2.17.0').ManagementClient;
 
-  if (!global.management) {
-    global.management = new ManagementClient({
+  // prettier-ignore
+  global.management = global.management || new ManagementClient({
       domain: auth0.domain,
       token: auth0.accessToken
     });
-  }
 
-  if (!global.isJson) {
-    global.isJson = isJson;
-  }
+  // prettier-ignore
+  global.isJson = global.isJson || function(headers) {
+      const contentType = headers['content-type'] || '';
+      return contentType.includes('application/json');
+    };
 
-  if (!global.requestPostAsync) {
-    global.requestPostAsync = requestPostAsync;
-  }
+  // prettier-ignore
+  global.requestPostAsync = global.requestPostAsync || function(options) {
+      return new Promise((resolve, reject) => {
+        request.post(options, (err, resp, body) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          if (!/^2/.test('' + resp.statusCode)) {
+            reject(new Error(JSON.stringify(body)));
+            return;
+          }
+          resolve(body);
+        });
+      });
+    };
 
   callback(null, user, context);
-
-  function isJson(headers) {
-    const contentType = headers['content-type'] || '';
-    return contentType.includes('application/json');
-  }
-
-  function requestPostAsync(options) {
-    return new Promise((resolve, reject) => {
-      request.post(options, (err, resp, body) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        if (!/^2/.test('' + resp.statusCode)) {
-          reject(new Error(JSON.stringify(body)));
-          return;
-        }
-        resolve(body);
-      });
-    });
-  }
 }
